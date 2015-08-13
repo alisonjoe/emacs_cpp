8b989a0ec137a731de1316deb3034a9ab04e3f03
alisonjoe <alisonjoe@163.com>
2015-08-13 15:52:09 +0800

    base

---
+++ elpa/connection-1.10/connection-autoloads.el
@@ -0,0 +1,18 @@
+;;; connection-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("connection-pkg.el" "connection.el") (21945
+;;;;;;  56884 746293 607000))
+
+;;;***
+
+(provide 'connection-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; connection-autoloads.el ends here
---
+++ elpa/wgrep-20141016.1656/wgrep-autoloads.el
@@ -0,0 +1,30 @@
+;;; wgrep-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (wgrep-setup) "wgrep" "wgrep.el" (21945 56732 235553
+;;;;;;  528000))
+;;; Generated autoloads from wgrep.el
+
+(autoload 'wgrep-setup "wgrep" "\
+Setup wgrep preparation.
+
+\(fn)" nil nil)
+
+(add-hook 'grep-setup-hook 'wgrep-setup)
+
+;;;***
+
+;;;### (autoloads nil nil ("wgrep-pkg.el") (21945 56732 269537 820000))
+
+;;;***
+
+(provide 'wgrep-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; wgrep-autoloads.el ends here
